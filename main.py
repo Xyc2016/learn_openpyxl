9555bccef1721998ce994c4a286e4ae382a3b1f2
xyc <986327386@qq.com>
2021-06-14 19:57:07 +0800

    mod

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import io
 from openpyxl import Workbook
-print(Workbook)
+from openpyxl.worksheet._write_only import WriteOnlyWorksheet
+from zipfile import ZipFile
+
+wb = Workbook(write_only=True)
+
+ws = wb.create_sheet()
+
+ws.append(["abcdefg"])
+
+file = io.BytesIO()
+
+wb.save(file)
+
+file.seek(0)
+with open("a.xlsx", "wb") as file1:
+    file1.write(file.read())
